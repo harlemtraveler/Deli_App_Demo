7eae26ee7dab1e441c26bfbd91cedf6841f5a794
Robot <tjk315@gmail.com>
2020-12-27 14:55:11 -0500

    Created "Add New Product" form

--- src/components/product_components/NewProduct.js
+++ src/components/product_components/NewProduct.js
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
-import { UserContext } from "../../App";
-import aws_exports from "../../aws-exports";
-import { API, graphqlOperation } from "aws-amplify";
+import React, { Component } from 'react';
+import { UserContext } from '../../App';
+import aws_exports from '../../aws-exports';
+//** Amplify & GraphQL Imports **//
+import { Storage, Auth, API, graphqlOperation } from "aws-amplify";
+import { PhotoPicker } from "aws-amplify-react";
 import { createProduct } from "../../graphql/mutations";
 //** MaterialUI Imports **//
 import { withStyles } from "@material-ui/core/styles";
@@ -20,20 +22,110 @@ import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 
 const initialState = {
-  description: "",
+  name: "",
   store: "",
-  file: {},
   price: "",
-  delivery: false,
-  tags: [],
   owner: "",
+  image: "",
+  description: "",
+  imagePreview: "",
+  delivery: false,
+  isUploading: false,
+  addProductDialog: false,
+  file: {},
+  options: [],
+  selectedTags: [],
+  tags: [
+    "Beverages",
+    "Entree",
+    "Snacks",
+    "Sides",
+    "Salads",
+    "Vegan"
+  ],
 };
 
 class NewProduct extends Component {
   state = { ...initialState };
 
+  handleAddProduct = async () => {
+    try {
+      this.setState({ isUploading: true });
+      const visibility = 'public';
+      const { indentityId } = await Auth.currentCredentials();
+      const { username } = await Auth.currentUserInfo();
+      const filename = `/${visibility}/${indentityId}/${Date.now()}-${this.state.image.name()}`;
+      const uploadedFile = await Storage.put(filename, this.state.image.file, {
+        contentType: this.state.image.type,
+        progressCallback: progress => {
+          console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+          const percentUploaded = Math.round((progress.loaded / progress.total) * 100);
+          this.setState({ percentUploaded });
+        }
+      });
+      const file = {
+        key: uploadedFile.key,
+        bucket: aws_exports.aws_user_files_s3_bucket,
+        region: aws_exports.aws_user_files_s3_bucket_region
+      };
+      const input = {
+        owner: username,
+        name: this.state.name,
+        description: this.state.description,
+        price: this.state.price,
+        delivery: this.state.delivery,
+        tags: this.state.selectedTags,
+        file
+      };
+      const result = await API.graphql(graphqlOperation(createProduct, { input }));
+      console.log(result);
+      console.info(`Created product id: ${result.data.createProduct.id}`);
+      this.setState({ ...initialState });
+      setTimeout(() => window.location.reload(), 2000);
+    } catch (err) {
+      console.error('[!] Error adding new product', err);
+    }
+  };
+
+  handleClickOpen = async () => {
+    this.setState({ addProductDialog: true });
+  };
+
+  handleClose = () => {
+    this.setState({ addProductDialog: false });
+    this.setState({ name: '', description: '', price: null, delivery: false, imagePreview: '', selectedTags: [] });
+  };
+
+  handleChange = event => {
+    event.preventDefault();
+    const targetName = event.target.name;
+    const targetValue = event.target.value;
+    this.setState({
+      ...targetName,
+      [targetName]: targetValue
+    });
+  };
+
+  handleFilterTags = query => {
+    const options = this.state.tags
+      .map(tag => ({ value: tag, label: tag }))
+      .filter(tag => tag.label.toLowerCase().includes(query.toLowerCase()));
+    this.setState({ options });
+  };
+
   render() {
     const { classes } = this.props;
+    const {
+      name,
+      tags,
+      price,
+      delivery,
+      description,
+      isUploading,
+      selectedTags,
+      imagePreview,
+      addProductDialog
+    } = this.state;
 
     return (
       <UserContext.Consumer>
@@ -49,12 +141,141 @@ class NewProduct extends Component {
             >
               <Grid item xs={12}>
                 <Paper style={{ height: '16em' }}>
-                  <Button variant={'outlined'} color={'primary'} onClick={() => {}} style={{ margin: '40px' }}>
+                  <Button variant={'outlined'} color={'primary'} onClick={this.handleClickOpen} style={{ margin: '40px' }}>
                     Add New Menu Item
                   </Button>
                 </Paper>
               </Grid>
             </Grid>
+
+            <Dialog
+              fullWidth
+              maxWidth={'sm'}
+              open={addProductDialog}
+              onClose={this.handleClose}
+              aria-labelledby={'form-dailog.title'}
+            >
+              <DialogTitle id={'form-dialog-title'}>New Product</DialogTitle>
+              <DialogContent className={classes.root}>
+                {/* Product Name Field */}
+                <TextField
+                  required
+                  autoFocus
+                  fullWidth
+                  id={'name'}
+                  value={name}
+                  name={'name'}
+                  label={'Name'}
+                  margin={'normal'}
+                  variant={'filled'}
+                  style={{ margin: 8 }}
+                  placeholder={'Product Name'}
+                  onChange={this.handleChange}
+                  InputLabelProps={{ shrink: true }}
+                />
+
+                {/* Product Description Field */}
+                <TextField
+                  required
+                  fullWidth
+                  multiline={true}
+                  id={'description'}
+                  value={description}
+                  name={'description'}
+                  label={'Description'}
+                  margin={'normal'}
+                  variant={'filled'}
+                  style={{ margin: 8 }}
+                  placeholder={'Product Description'}
+                  onChange={this.handleChange}
+                  InputLabelProps={{ shrink: true }}
+                />
+
+                {/* Product Price Field */}
+                <TextField
+                  required
+                  fullWidth
+                  id={'price'}
+                  value={price}
+                  name={'price'}
+                  label={'Price'}
+                  margin={'normal'}
+                  variant={'filled'}
+                  style={{ margin: 8 }}
+                  placeholder={'Product Price'}
+                  onChange={this.handleChange}
+                  InputLabelProps={{ shrink: true }}
+                />
+
+                {/* Image Pick & Upload */}
+                <Box>
+                  {imagePreview && (
+                    <img
+                      src={imagePreview}
+                      alt={'Product Preview'}
+                      className={'image-preview'}
+                    />
+                  )}
+                  <PhotoPicker
+                    preview={'hidden'}
+                    title={'Product Image'}
+                    onPick={file => this.setState({ image: file })}
+                    onLoad={photoUrl => this.setState({ imagePreview: photoUrl })}
+                    theme={{
+                      formContainer: {
+                      margin: 0,
+                      padding: "0.8em"
+                      },
+                      sectionBody: {
+                        margin: 0,
+                        width: "250px"
+                      },
+                      formSection: {
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center"
+                      },
+                      sectionHeader: {
+                        padding: "0.2em",
+                        color: "var(--darkAmazonOrange)"
+                      },
+                      amplifyButton: {
+                        display: "none"
+                      }
+                    }}
+                  />
+                </Box>
+
+                <FormControl className={classes.formControl}>
+                  <InputLabel id={'category-tags'}>Tags</InputLabel>
+                  <Select
+                    multiple
+                    id={'addTags'}
+                    labelId={'addTags'}
+                    value={selectedTags}
+                    name={'selectedTags'}
+                    onChange={this.handleChange}
+                  >
+                    <MenuItem value={''} />
+                    {tags.map(tag => (
+                      <MenuItem key={tag} value={tag}>{tag}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+
+              </DialogContent>
+              <DialogActions>
+                <Button color={'secondary'} onClick={this.handleClose}>Cancel</Button>
+                <Button
+                  color={'primary'}
+                  onClick={this.handleAddProduct}
+                  disabled={!name || !description || isUploading}
+                >
+                  {isUploading ? "Uploading..." : "Add Product"}
+                </Button>
+              </DialogActions>
+            </Dialog>
           </>
         }
       </UserContext.Consumer>
